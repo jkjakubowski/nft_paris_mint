9bc8ce2d43a9e3c524345eb2429666ce7d50a98d
Draktors <hespelenzo@gmail.com>
2022-01-18 22:30:42 +0100

    voilà

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { css } from "@emotion/react";
+import video from "./video.mp4";
+
+const style = css`
+  color: #5835e9;
+  font-size: 5rem;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div
+        css={css`
+          height: 100vh;
+          width: 100vw;
+        `}
+      >
+        <h1 css={style}>Special gift!</h1>
+        <h2
+          css={css`
+            font-size: 2rem;
+            color: #ffffff;
+            margin-top: 1rem;
+          `}
         >
-          Learn React
-        </a>
-      </header>
+          Thanks for attending the first edition of NFT Paris!
+        </h2>
+        <video controls width="300" autoPlay muted>
+          <source src={video} type="video/mp4" />
+        </video>
+      </div>
     </div>
   );
 }
